7fb94e5a82e0700a2ca931bcf584dadb0f07c865
siddharth shah <siddharthshah3030@gmail.com>
2019-02-24 02:15:35 +0530

    addign home section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navBar.js';
+import Home from './components/home.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar/>
+        <Home/>
       </div>
     );
   }
---
+++ src/components/home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+// import { Navbar } from 'react-bulma-components';
+import { Section, Container ,Heading  } from 'react-bulma-components';
+// import { Navbar } from 'react-bulma-components';
+// import { Navbar } from 'react-bulma-components';
+// import { Button } from 'react-bulma-components';
+
+export default () => (
+    <div>
+
+
+
+<Section size="medium">
+<Container>
+  <Heading>Section</Heading>
+  <Heading subtitle>
+    A simple container to divide your page into <strong>sections</strong>, like the one you are currently
+    reading
+  </Heading>
+</Container>
+</Section>
+    </div>
+)
