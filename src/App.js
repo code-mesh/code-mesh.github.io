bdd3ff06726546b58384f5aa4555a912c23224a1
siddharth shah <siddharthshah3030@gmail.com>
2019-02-24 01:16:56 +0530

    stashing before ejection of npm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-   
+        <NavBar/>
+        SiD
       </div>
     );
   }
---
+++ src/components/navBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+
+// import { Button } from 'react-bulma-components';
+
+// import { Field, Control } from 'react-bulma-components/lib/components/form';
+// import Button from 'react-bulma-components/lib/components/button';
+// import Navbar from 'react-bulma-components/lib/components/navbar';
+
+export default () => (
+  <Button color="primary">My Bulma button</Button>
+)
